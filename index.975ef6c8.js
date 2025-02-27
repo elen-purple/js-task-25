ec5253c5fed567861b44e6ae8059e34b71736ce6
elen-purple <anna.havrylova.2011@gmail.com>
2025-02-27 13:09:24 +0200

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -596,6 +596,27 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"8lqZg":[function(require,module,exports,__globalThis) {
+const form = document.querySelector("#form");
+const submitBtn = document.querySelector("#submit-btn");
+const backdrop = document.querySelector("#backdrop");
+const closeBtn = document.querySelector("#close-btn");
+const page = document.querySelector("body");
+const user = {};
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    user.name = form.querySelector("#form-name").value;
+    user.email = form.querySelector("#form-email").value;
+    user.message = form.querySelector("#form-message").value;
+    form.querySelector("#form-name").value = "";
+    form.querySelector("#form-email").value = "";
+    form.querySelector("#form-message").value = "";
+    backdrop.classList.toggle("is-hidden");
+    page.classList.toggle("no-scroll");
+});
+closeBtn.addEventListener("click", ()=>{
+    backdrop.classList.toggle("is-hidden");
+    page.classList.toggle("no-scroll");
+});
 
 },{}]},["9mu7C","8lqZg"], "8lqZg", "parcelRequire94c2")
 
